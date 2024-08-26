3e15d5d55dc5d6fc40589e1538def69e800d6352
Odair H <odair.sp@hotmail.com>
2024-08-26 15:18:43 -0300

    Iniciando com Mockito

---
+++ src/test/java/javamockito/ListaTeste.java
@@ -0,0 +1,30 @@
+package javamockito;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+
+import java.util.List;
+
+
+@ExtendWith(MockitoExtension.class)
+public class ListaTeste {
+   @Mock
+
+    private List<String> letras;
+
+
+    @Test
+    void adicionarItemInList() {
+
+        Mockito.when(letras.getFirst()).thenReturn("B");
+
+        Assertions.assertEquals("B", letras.getFirst());
+
+    }
+
+}
